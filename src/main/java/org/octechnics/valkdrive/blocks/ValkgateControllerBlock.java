3a23b67675353a58fa38adc382d296c8145e5db6
Andrey <pythonhunter146@gmail.com>
2022-07-20 00:06:20 +0500

    Started working on custom stargate model

--- src/main/java/org/octechnics/valkdrive/blocks/ValkgateControllerBlock.java
+++ src/main/java/org/octechnics/valkdrive/blocks/ValkgateControllerBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.BlockHitResult;
@@ -78,6 +79,11 @@ public class ValkgateControllerBlock extends Block implements EntityBlock {
         } else {
             assert false;
         }
+    }
+    
+    @Override
+    public RenderShape getRenderShape(final BlockState state) {
+        return RenderShape.MODEL;
     }
     
     @Override
