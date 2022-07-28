184ef52c93491f9b1b64764488164eade4fc328a
Andrey <pythonhunter146@gmail.com>
2022-07-29 00:31:15 +0500

    Updated model, found way to texture it

--- src/main/java/org/octechnics/valkdrive/ValkgateEntityModel.java
+++ src/main/java/org/octechnics/valkdrive/ValkgateEntityModel.java
@@ -1,6 +1,6 @@
 package org.octechnics.valkdrive;
 
-import net.minecraft.client.model.geom.ModelPart.Polygon;
+import net.minecraft.client.model.geom.ModelPart.Polygon; // AccessTransformed
 import net.minecraft.client.model.geom.ModelPart.Vertex;
 import net.minecraft.client.color.block.BlockColors;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -40,26 +40,56 @@ public class ValkgateEntityModel<T extends ValkgateEntity> extends EntityModel<T
         Vertex back_rgt_t = new Vertex(64.0F,34.0F,0.0F,   64.0F,34.0F);
         Vertex back_top_r = new Vertex(50.0F,48.0F,0.0F,   50.0F,48.0F);
         
+        Vertex back_sm_cent  = new Vertex(40.0F,24.0F,2.0F,   40.0F,24.0F);
+        Vertex back_sm_top_l = new Vertex(31.0F,46.0F,2.0F,   31.0F,46.0F);
+        Vertex back_sm_lft_t = new Vertex(18.0F,33.0F,2.0F,   18.0F,33.0F);
+        Vertex back_sm_lft_b = new Vertex(18.0F,15.0F,2.0F,   18.0F,15.0F);
+        Vertex back_sm_bot_l = new Vertex(31.0F,2.0F, 2.0F,   31.0F,2.0F);
+        Vertex back_sm_bot_r = new Vertex(49.0F,2.0F, 2.0F,   49.0F,2.0F);
+        Vertex back_sm_rgt_b = new Vertex(62.0F,15.0F,2.0F,   62.0F,15.0F);
+        Vertex back_sm_rgt_t = new Vertex(62.0F,33.0F,2.0F,   62.0F,33.0F);
+        Vertex back_sm_top_r = new Vertex(49.0F,46.0F,2.0F,   49.0F,46.0F);
+        
         quads = new Polygon[] {
             new Polygon(
               new Vertex[]{back_cent,back_top_l,back_lft_t,back_lft_b},
-              1.0F,1.0F,1.0F,1.0F,1.0F,1.0F,
+              8.0F,8.0F,16.0F,16.0F,16.0F,16.0F,
               false, Direction.NORTH
             ),
             new Polygon(
               new Vertex[]{back_cent,back_lft_b,back_bot_l,back_bot_r},
-              1.0F,1.0F,1.0F,1.0F,1.0F,1.0F,
+              8.0F,0.0F,16.0F,8.0F,16.0F,16.0F,
               false, Direction.NORTH
             ),
             new Polygon(
               new Vertex[]{back_cent,back_bot_r,back_rgt_b,back_rgt_t},
-              1.0F,1.0F,1.0F,1.0F,1.0F,1.0F,
+              0.0F,8.0F,8.0F,16.0F,16.0F,16.0F,
               false, Direction.NORTH
             ),
             new Polygon(
               new Vertex[]{back_cent,back_rgt_t,back_top_r,back_top_l},
-              1.0F,1.0F,1.0F,1.0F,1.0F,1.0F,
+              0.0F,0.0F,8.0F,8.0F,16.0F,16.0F,
               false, Direction.NORTH
+            ),
+            new Polygon(
+              new Vertex[]{back_sm_cent,back_sm_top_l,back_sm_lft_t,back_sm_lft_b},
+              0.0F,0.0F,8.0F,8.0F,16.0F,16.0F,
+              false, Direction.SOUTH
+            ),
+            new Polygon(
+              new Vertex[]{back_sm_cent,back_sm_lft_b,back_sm_bot_l,back_sm_bot_r},
+              0.0F,8.0F,8.0F,16.0F,16.0F,16.0F,
+              false, Direction.SOUTH
+            ),
+            new Polygon(
+              new Vertex[]{back_sm_cent,back_sm_bot_r,back_sm_rgt_b,back_sm_rgt_t},
+              8.0F,0.0F,16.0F,8.0F,16.0F,16.0F,
+              false, Direction.SOUTH
+            ),
+            new Polygon(
+              new Vertex[]{back_sm_cent,back_sm_rgt_t,back_sm_top_r,back_sm_top_l},
+              8.0F,8.0F,16.0F,16.0F,16.0F,16.0F,
+              false, Direction.SOUTH
             )
         };
     }
