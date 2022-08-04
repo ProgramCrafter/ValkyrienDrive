ae64f5f004264d8ae048dc2242328ca8a96f51cc
Andrey <pythonhunter146@gmail.com>
2022-08-04 18:08:35 +0500

    Entity is spawned by controller tile and correctly doesn't persist world reload

--- src/main/java/org/octechnics/valkdrive/ValkgateEntity.java
+++ src/main/java/org/octechnics/valkdrive/ValkgateEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.core.Direction;
 
 import javax.annotation.Nullable;
@@ -83,5 +84,16 @@ public class ValkgateEntity extends HangingEntity {
     @Override
     public Packet<?> getAddEntityPacket() {
         return new ClientboundAddEntityPacket(this, this.getType(), this.direction.get3DDataValue(), this.getPos());
+    }
+    
+    @Override public boolean shouldBeSaved() {
+        return false;
+    }
+    @Override public boolean save(CompoundTag tag) {
+        return false;
+    }
+    
+    @Override public boolean canChangeDimensions() {
+        return false;
     }
 }
--- src/main/java/org/octechnics/valkdrive/tile/ValkgateControllerBE.java
+++ src/main/java/org/octechnics/valkdrive/tile/ValkgateControllerBE.java
@@ -2,11 +2,15 @@ package org.octechnics.valkdrive.tile;
 
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.core.BlockPos;
 
+import org.octechnics.valkdrive.BlocksItemsRegistryHelper;
 import org.octechnics.valkdrive.ValkyrienDrive;
+import org.octechnics.valkdrive.ValkgateEntity;
 
 public class ValkgateControllerBE extends BlockEntity {
     public boolean valid = false;
@@ -14,8 +18,31 @@ public class ValkgateControllerBE extends BlockEntity {
     public int     dy    = 0xFF;
     public int     dz    = 0xEE;
     
+    private ValkgateEntity entity = null;
+    
     public ValkgateControllerBE(final BlockPos pos, final BlockState state) {
         super(ValkyrienDrive.registry_blocks.VALKGATE_CONTR_TILE.get(), pos, state);
+    }
+    
+    @Override
+    public void setLevel(Level level) throws IllegalStateException {
+        super.setLevel(level);
+        
+        if (level != null && !level.isClientSide()) {
+            if (entity != null) {
+                entity.discard();
+                entity = null;
+            }
+            
+            final ServerLevel world = (ServerLevel)level;
+            if (world == null) {assert false;}
+            
+            entity = new ValkgateEntity(BlocksItemsRegistryHelper.VALKGATE_ENTITY.get(), world);
+            entity.moveTo(this.getBlockPos().getX(), this.getBlockPos().getY(), this.getBlockPos().getZ(), 0, 0);
+            if (!world.tryAddFreshEntityWithPassengers(entity)) {
+                throw new IllegalStateException("Failed to create entity bound to block entity");
+            }
+        }
     }
     
     private boolean validateDelta(int adx, int ady, int adz) {
